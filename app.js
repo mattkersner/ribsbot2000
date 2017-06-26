f1e0757a929d81bf8996a6152c204d808dce0f11
Jason Andrada <jpa323@nyu.edu>
2017-06-26 16:52:27 -0400

    adding Luke and Gainor as TAs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var slackbot = require('./slackbot-new');
 var fs = require('fs');
 
 var botKey = "xoxb-159703536768-pd7LMDEykTZER5m5PN6bh7gD";
-var taIDs = "U361Q3GPP,U2JU26MPS,U42S89JCX,U2BGW8TS7,U3K0RFJAY".split(",");
+var taIDs = "U361Q3GPP,U2JU26MPS,U42S89JCX,U2BGW8TS7,U3K0RFJAY,U5Q1G6L91,U4A9PV49F".split(",");
 
 var bot = new slackbot(botKey);
 
@@ -17,4 +17,3 @@ for (var key in easterEggs) {
 }
 
 bot.connect();
-
